5fa159f077dd2713630891639cc12b1ad69c00ee
Csaba Hermann <hcsabi98@gmail.com>
2020-09-27 12:56:34 +0200

    Read and plot the dataset

--- lesson2/linearRegression.py
+++ lesson2/linearRegression.py
@@ -1,3 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Linear regression example
+
+data = np.loadtxt("lesson2/data1.csv", delimiter=",")
+x = data[:,0]
+y = data[:,1]
+
+plt.plot(x,y, "rx")
+plt.show()
+
+print("Plotting data")
