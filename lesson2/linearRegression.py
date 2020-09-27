00ebf3f41aa874cc5e51cb773387275a91c1d895
Csaba Hermann <hcsabi98@gmail.com>
2020-09-27 20:30:29 +0200

    Visualizing theta

--- lesson2/linearRegression.py
+++ lesson2/linearRegression.py
@@ -16,6 +16,7 @@ x = np.reshape(x, (m, 1))
 y = np.reshape(y, (m, 1))
 
 print("Plotting data")
+plt.figure()
 plt.plot(x,y, "rx")
 
 x = np.append(np.ones(shape=(m, 1)), x, axis=1) # Add a column of ones to x
@@ -48,5 +49,51 @@ print(' -3.6303\n  1.1664\n')
 # Plot the linear fit
 plt.plot(x[:,1], np.dot(x,theta), 'b-')
 plt.legend(["Training data", "Linear regression"])
+plt.draw()
+
+# Predict values for population sizes of 35,000 and 70,000
+predict1 = np.dot([1, 3.5], theta)
+print("For population = 35,000, we predict a profit of " + str(predict1 * 10000))
+predict2 = np.dot([1, 7], theta)
+print('For population = 70,000, we predict a profit of ' +  str(predict2 * 10000))
+
+
+# ============= Part 4: Visualizing J(theta_0, theta_1) =============
+print('Visualizing J(theta_0, theta_1) ...\n')
+
+# Grid over which we will calculate J
+theta0Vals = np.linspace(-10, 10, 100)
+theta1Vals = np.linspace(-1, 4, 100)
+
+# initialize jVals to a matrix of 0's
+jVals = np.zeros((len(theta0Vals), len(theta1Vals)))
+
+# Fill out jVals
+for i in range(len(theta0Vals)):
+    for j in range(len(theta1Vals)):
+	    t = [theta0Vals[i], theta1Vals[j]]
+	    jVals[i,j] = computeCost(x, y, t)
+
+# Because of the way meshgrids work in the surf command, we need to
+# transpose jVals before calling surf, or else the axes will be flipped
+JVals = np.transpose(jVals)
+
+print("jVals")
+print(jVals)
+# Surface plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(theta0Vals, theta1Vals, jVals)       ###### BUG!!
+plt.xlabel(r'$\theta_0$')
+plt.ylabel(r'$\theta_1$')
+plt.draw()
+
+# Contour plot
+plt.figure()
+# Plot jVals as 15 contours spaced logarithmically between 0.01 and 100
+plt.contour(theta0Vals, theta1Vals, jVals, np.logspace(-2, 3, 20))
+plt.xlabel(r'$\theta_0$')
+plt.ylabel(r'$\theta_1$')
+plt.plot(theta[0], theta[1], 'rx', 10, 2)
+
 plt.show()
-# don't overlay any more plots on this figure
